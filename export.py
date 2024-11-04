d8d9258db1648640f18d04b235b35c1df64137b0
Markus Bukowski <mrbuk@users.noreply.github.com>
2024-11-04 17:36:45 +0100

    use object dict to store data

--- export.py
+++ export.py
@@ -44,7 +44,7 @@ class ExportService:
                 self.export_request(r)
 
     def export_request(self, r):
-        raw_data = r
+        raw_data = r.toDict()
         try:
             result = self.client.create_data_export(r)
             id = result.get("dataExportId", "UNKNOWN")
